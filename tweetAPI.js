ec86268d6feec3aeaef0a2df4104f45d4978ad0b
Jaziel Isai <code.isai.dev@gmail.com>
2021-11-28 16:56:23 -0600

    update index.js

--- tweetAPI.js
+++ tweetAPI.js
@@ -4,39 +4,32 @@
 // Access Token Secret --> afRPnEoR9BYPctcWyFBGV5pXvrOPwCYyUKFlDqo7Vxhnv
 // Bearer Token --> AAAAAAAAAAAAAAAAAAAAAKJ%2BVwEAAAAAI3vyK2xUQwG3uXpvPgiKDIrzcxo%3D0wHYeBgluKuAgpiV6AiE9C1LspKgeDEUM97UySukSynYrnMwtt
 //
- var twit = require('twit');
+var twit = require("twit");
+
+var Twitter = new twit({
+  consumer_key: "sju4nRN0oJjBn4T3aTc3AuUfw",
+  consumer_secret: "ccYmczcQdooe0aImCOVsI0JK6ySiwTCHh90Bk7DkK19urBxsVi",
+  access_token: "1453195308591050754-EZm3fY86WQLIrh9cBX3UVwqihv9Ztp",
+  access_token_secret: "afRPnEoR9BYPctcWyFBGV5pXvrOPwCYyUKFlDqo7Vxhnv",
+  timeout_ms: 60 * 1000, //
+  strictSSL: true, //opciones
+});
+
+let params = {};
+
+Twitter.post("statuses/update", params, (err, data) => {
+  try {
+    if (err) {
+      return reject(err);
+    }
+    return resolve(data);
+  } catch (err) {
+    return reject(err);
+  }
+});
 
 
- var Twitter =  new twit({
-    consumer_key: 'sju4nRN0oJjBn4T3aTc3AuUfw',
-    consumer_secret: 'ccYmczcQdooe0aImCOVsI0JK6ySiwTCHh90Bk7DkK19urBxsVi',
-    access_token: '1453195308591050754-EZm3fY86WQLIrh9cBX3UVwqihv9Ztp',
-    access_token_secret: 'afRPnEoR9BYPctcWyFBGV5pXvrOPwCYyUKFlDqo7Vxhnv',
-    timeout_ms: 60*1000, //
-    strictSSL: true, //opciones 
- });
- 
- function postTweet(id){
-     return new Promise( (resolve, reject) => {
-        let params = {
-            id,
-        };
 
-        Twitter.post('statuses/update', params, (err, data) => {
-            try{
-                if(err) {
-                    return reject(err);
-                }
-                return resolve(data);
-            }catch(err){
-                return reject(err);
-            }
-        });
-
-    });
-
-}
- 
 /*
 Twitter.post('statuses/update', {status: 'Trends book'})
 .then((tweet) => {
